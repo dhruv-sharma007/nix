d939e8eab576f8e06b20b2f5d0b06ce215f9dfd8
Dhruv <dhruv.freelancerrr@gmail.com>
2025-03-18 21:47:45 +0530

    My first commit

---
+++ main.nix
@@ -0,0 +1,20 @@
+#I am Learning nix to use nix os this is first time i am coding in Nix
+
+let 
+name = "Dhruv";
+age = 18;
+isAdult = true;
+in 
+
+if isAdult then "You can vote" else "You are too young" 
+
+{
+  x = 123;
+  text = "Hello";
+  y = f { bla = 456; };
+}
+
+let bar = "foo"; in
+{ ${bar} = 123; }.foo
+
+
