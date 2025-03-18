270db4574b1f32c38586005b850acbb87c1bdb3e
Dhruv <dhruv.freelancerrr@gmail.com>
2025-03-18 22:56:10 +0530

    learn something new

--- main.nix
+++ main.nix
@@ -1,20 +1,30 @@
-#I am Learning nix to use nix os this is first time i am coding in Nix
+# #I am Learning nix to use nix os this is first time i am coding in Nix
 
-let 
-name = "Dhruv";
-age = 18;
-isAdult = true;
-in 
+# let 
+# name = "Dhruv";
+# age = 18;
+# isAdult = true;
+# in 
 
-if isAdult then "You can vote" else "You are too young" 
+# if isAdult then "You can vote" else "You are too young" 
 
+# {
+#   x = 123;
+#   text = "Hello";
+#   y = f { bla = 456; };
+# }
+
+# let bar = "foo"; in
+# { ${bar} = 123; }.foo
+
+
+# let
+#   x = "foo";
+#   y = "bar";
+# in x + y
+
+let x = 123; in 
 {
-  x = 123;
-  text = "Hello";
-  y = f { bla = 456; };
+  inherit x;
+  y = 456;
 }
-
-let bar = "foo"; in
-{ ${bar} = 123; }.foo
-
-
